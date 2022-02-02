799182c8ba4f313bd56c9c1e24c176c768c5b5cd
Joan <guigomene15@gmail.com>
2022-02-02 17:45:52 -0300

    C++ ex04

---
+++ ex03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string characterName = "George";
+    int characterAge = 70;
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He has " << characterAge << " y old" << endl;
+    cout << "He like the name " << characterName << endl;
+    cout << "But didn't like being " << characterAge << endl;
+
+    return 0;
+}
